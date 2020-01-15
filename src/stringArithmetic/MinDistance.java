937f9fc8f0a7813c279b9d5bdc7e1de7cf0b2c33
FenglinBi <515186469@qq.com>
2020-01-15 20:35:07 +0800

    min distance

--- src/stringArithmetic/MinDistance.java
+++ src/stringArithmetic/MinDistance.java
@@ -1,5 +1,8 @@
 package stringArithmetic;
 
+import java.util.List;
+import java.util.Map;
+
 /*给定两个单词 word1 和 word2，计算出将 word1 转换成 word2 所使用的最少操作数 。
 
         你可以对一个单词进行如下三种操作：
@@ -26,4 +29,37 @@ package stringArithmetic;
         exention -> exection (将 'n' 替换为 'c')
         exection -> execution (插入 'u')*/
 public class MinDistance {
+
+
+    public static void main(String[] args) {
+        String word1 = "horse";
+        String word2 = "ros";
+        System.out.println(minDistance(word1, word2));
+    }
+
+    public static int minDistance(String word1, String word2) {
+        int length1 = word1.length();
+        int length2 = word2.length();
+
+        int[][] dp = new int[length1 + 1][length2 + 1];
+
+        for (int i = 1; i <= length1; ++i) {
+            dp[i][0] = i;
+        }
+        for (int i = 1; i <= length2; ++i) {
+            dp[0][i] = i;
+        }
+
+        for (int i = 1; i <= length1; i++) {
+            for (int j = 1; j <= length2; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+            }
+        }
+        return dp[length1][length2];
+
+    }
 }

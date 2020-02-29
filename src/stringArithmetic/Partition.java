2c526cf13eeb4068691b8ecac799aebe1cd50b66
bifenglin <515186469@qq.com>
2020-02-29 18:37:42 +0800

    delete readme

--- src/stringArithmetic/Partition.java
+++ src/stringArithmetic/Partition.java
@@ -22,37 +22,52 @@ public class Partition {
         int len = input.length();
         Stack path = new Stack();
         List<List<String>> res = new ArrayList<>();
-        BackTrace(0, input, len, path, res);
+        backtracking(input, 0, len, path, res);
         res.forEach(index -> {
             System.out.println(index);
         });
 
     }
-
-
-    public static void BackTrace(int start, String s, int len, Stack path, List<List<String>> res) {
+    /**
+     * @param s
+     * @param start 起始字符的索引
+     * @param len   字符串 s 的长度，可以设置为全局变量
+     * @param path  记录从根结点到叶子结点的路径
+     * @param res   记录所有的结果
+     */
+    private static void backtracking(String s, int start, int len, Stack path, List<List<String>> res) {
         if (start == len) {
             res.add(new ArrayList<>(path));
             return;
         }
 
         for (int i = start; i < len; i++) {
-            if (!isPalindrome(s, start, i))
+            if (!checkPalindrome(s, start, i)) {
                 continue;
+            }
+
             path.add(s.substring(start, i + 1));
-            BackTrace(start + 1, s, len, path, res);
+            backtracking(s, i + 1, len, path, res);
             path.pop();
         }
     }
 
-    public static boolean isPalindrome(String s, int start, int end) {
-        while (start < end) {
-            if (s.charAt(start) == s.charAt(end)) {
-                start++;
-                end--;
-            } else {
+    /**
+     * 这一步的时间复杂度是 O(N)，因此，可以采用动态规划先把回文子串的结果记录在一个表格里
+     *
+     * @param str
+     * @param left  子串的左边界，可以取到
+     * @param right 子串的右边界，可以取到
+     * @return
+     */
+    private static boolean checkPalindrome(String str, int left, int right) {
+        // 严格小于即可
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
                 return false;
             }
+            left++;
+            right--;
         }
         return true;
     }
